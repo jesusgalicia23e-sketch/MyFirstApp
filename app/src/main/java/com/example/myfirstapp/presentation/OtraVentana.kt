d1d67c8dbcc4aedbfa9bc19e759eb0c6b4d4723a
brenel <jesus.galicia.23e@utzmg.edu.mx>
2025-10-02 08:52:36 -0600

    sixth commit

--- app/src/main/java/com/example/myfirstapp/presentation/OtraVentana.kt
+++ app/src/main/java/com/example/myfirstapp/presentation/OtraVentana.kt
@@ -1,13 +1,45 @@
 package com.example.myfirstapp.presentation
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle;
 import androidx.activity.ComponentActivity;
+import androidx.core.app.ActivityCompat
 import com.example.myfirstapp.R;
 
 
-class OtraVentana: ComponentActivity() {
+abstract class OtraVentana: ComponentActivity(), SensorEventListener {
+    private lateinit var sensorManager:SensorManager
+    private var sensor: Sensor?=null
+    private var sensorType = Sensor.TYPE_ACCELEROMETER
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        sensorManager=getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensor= SensorManager.getDefaultSensor(sensorType)
         setContentView(R.layout.otra_ventana)
+    }
+
+    private fun startSensor(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BODY_SENSORS)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.BODY_SENSORS), 1001)
+        }
+
+        if (sensor!=null){
+            sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+
+    }
+
+    override fun onSensorChanged(p0: SensorEvent?) {
+
     }
 }
