449e76bc03222da9335276dde9bd2c6b3b6cccc2
brenel <jesus.galicia.23e@utzmg.edu.mx>
2025-10-08 11:36:53 -0600

    eleventh commit

--- appwearos/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ appwearos/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myfirstapp
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -15,14 +16,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myfirstapp.ui.theme.MyFirstAppTheme
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.Tasks
 import com.google.android.gms.wearable.CapabilityClient
 import com.google.android.gms.wearable.CapabilityInfo
 import com.google.android.gms.wearable.DataClient
 import com.google.android.gms.wearable.DataEventBuffer
 import com.google.android.gms.wearable.MessageClient
 import com.google.android.gms.wearable.MessageEvent
+import com.google.android.gms.wearable.Wearable
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity(), CoroutineScope by MainScope(), DataClient.OnDataChangedListener, MessageClient.OnMessageReceivedListener,
 CapabilityClient.OnCapabilityChangedListener{
@@ -41,6 +47,23 @@ CapabilityClient.OnCapabilityChangedListener{
         conectar.setOnClickListener {
             if (!deviceConnected){
 
+            }
+        }
+    }
+
+    private fun getNodes(context: Context){
+        launch(Dispatchers.Default){
+            val nodeList= Wearable.getNodeClient(context).connectedNodes
+            try {
+                val  nodes= Tasks.await(nodeList)
+                for (node in nodes){
+                    Log.d("NODO", node.toString())
+                    Log.d("NODO", "El id del nodo es:  ${node.id}")
+                    nodeID=node.id
+                    deviceConnected=true
+                }
+            } catch (exception: Exception){
+                Log.d("ERROR en el nodo", exception.toString())
             }
         }
     }
